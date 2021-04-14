dc9d4cc2d138d7e7a3021608913479b421ee4409
Tamer <tamerardaal@gmail.com>
2021-04-14 13:23:26 +0300

    db_Helper tanımlandı

--- lib/db/db_Helper.dart
+++ lib/db/db_Helper.dart
@@ -4,3 +4,17 @@ import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:e_ticaret_app/models/Urun.dart';
 
+class dbHelper{
+  String tblProduct = "Products";
+  String colID = "id";
+  String colName = "Name";
+  String colDescription = "Description";
+  String colPrice = "Price";
+
+  static final dbHelper _dbHelper = dbHelper._internal();
+  factory dbHelper(){
+    return _dbHelper;
+  }
+
+  dbHelper._internal();
+}
