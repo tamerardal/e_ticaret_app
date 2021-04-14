16351243d503abaeface572e387fe1834e37f0f7
tamerardal <tamerardaal@gmail.com>
2021-04-14 13:14:49 +0300

    Initial commit

---
+++ lib/db/db_Helper.dart
@@ -0,0 +1,6 @@
+import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:e_ticaret_app/models/Urun.dart';
+
---
+++ lib/models/Urun.dart
@@ -0,0 +1,11 @@
+class Urun{
+  int _id;
+  String _ad;
+  String _aciklama;
+  double _fiyat;
+
+  Urun(this._ad, this._aciklama, this._fiyat);
+  Urun.withId(this._id, this._ad, this._aciklama, this._fiyat);
+
+
+}
