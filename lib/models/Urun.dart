f20dc0616a77661404470092d2e26901dd95edeb
Tamer <tamerardaal@gmail.com>
2021-04-15 10:43:53 +0300

    getter ve setter oluşturuldu

--- lib/models/Urun.dart
+++ lib/models/Urun.dart
@@ -1,11 +1,29 @@
 class Urun{
   int _id;
-  String _ad;
-  String _aciklama;
-  double _fiyat;
+  String _name;
+  String _description;
+  double _price;
 
-  Urun(this._ad, this._aciklama, this._fiyat);
-  Urun.withId(this._id, this._ad, this._aciklama, this._fiyat);
+  Urun(this._name, this._description, this._price);
+  Urun.withId(this._id, this._name, this._description, this._price);
 
+  int get id => _id;
+  set id(int value) {
+    _id = value;
+  }
 
+  String get name => _name;
+  set name(String value) {
+    _name = value;
+  }
+
+  String get description => _description;
+  set description(String value) {
+    _description = value;
+  }
+
+  double get price => _price;
+  set price(double value) {
+    _price = value;
+  }
 }
