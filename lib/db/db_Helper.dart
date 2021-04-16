1946d0cd2f1800817c36d7bc7bcec9f354b8b32e
Tamer <tamerardaal@gmail.com>
2021-04-16 15:06:15 +0300

    . hata giderildi

--- lib/db/db_Helper.dart
+++ lib/db/db_Helper.dart
@@ -1,20 +1,19 @@
-//import 'dart:html';
 import 'package:sqflite/sqflite.dart';
 import 'dart:async';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:e_ticaret_app/models/Urun.dart';
 
-class dbHelper{
-  String tblProduct = "Products";
+class DbHelper{
+  String tblProduct = "products";
   String colID = "id";
-  String colName = "Name";
-  String colDescription = "Description";
-  String colPrice = "Price";
+  String colName = "name";
+  String colDescription = "description";
+  String colPrice = "price";
 
-  static final dbHelper _dbHelper = dbHelper._internal();
+  static final DbHelper _dbHelper = DbHelper._internal();
 
-  factory dbHelper(){
+  factory DbHelper(){
     return _dbHelper;
   }
 
@@ -29,17 +28,17 @@ class dbHelper{
   Future<Database> createdb() async{
     Directory directory = await getApplicationDocumentsDirectory();
     String _path = directory.path + "eticaret.db";
-    var eTicaretDB = await openDatabase(_path, version: 1, onCreate: create);
-    return eTicaretDB;
+    var eTicaretDb = await openDatabase(_path, version: 1, onCreate: create);
+    return eTicaretDb;
   }
 
-  dbHelper._internal();
+  DbHelper._internal();
 
   void create(Database db, int version) async{
-    await db.execute("Create Table $tblProduct($colID integer primary key, $colName text, $colDescription text, $colPrice int");
+    await db.execute("Create Table $tblProduct($colID integer primary key, $colName text, $colDescription text, $colPrice int)");
   }
 
-  Future<int> add(Urun product) async{
+  Future<int> add(Product product) async{
     Database db = await this.db;
     var result = await db.insert(tblProduct, product.doMap());
     return result;
@@ -51,7 +50,7 @@ class dbHelper{
     return result;
   }
 
-  Future<int> update(Urun product) async{
+  Future<int> update(Product product) async{
     Database db = await this.db;
     var result = await db.update(tblProduct, product.doMap());
     return result;
