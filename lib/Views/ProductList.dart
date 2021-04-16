8bd4c54e49cc7b0ba85f16bb2530f60d117e280e
Tamer <tamerardaal@gmail.com>
2021-04-16 15:06:47 +0300

    .

--- lib/Views/ProductList.dart
+++ lib/Views/ProductList.dart
@@ -1,55 +1,69 @@
+import 'package:e_ticaret_app/Views/InsertProduct.dart';
+import 'package:e_ticaret_app/Views/ProductDetail.dart';
 import 'package:e_ticaret_app/db/db_Helper.dart';
 import 'package:e_ticaret_app/models/Urun.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ProductList extends StatefulWidget{
+class ProductList extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return ProductListState();
   }
 }
 
-class ProductListState extends State{
-  dbHelper dbhelper = new dbHelper();
-  List<Urun> products;
+class ProductListState extends State {
+  DbHelper dbHelper = new DbHelper();
+  List<Product> products;
   int productValue = 0;
   @override
   Widget build(BuildContext context) {
-    if(products == null){
-      products = new List<Urun>();
+    if (products == null) {
+      products = new List<Product>();
       getProducts();
     }
-    return ListView.builder(
-      itemCount: productValue,
-      itemBuilder: (BuildContext context, int position){
-        return Card(
-          color: Colors.lightBlueAccent,
-          elevation: 2,
-          child: ListTile(
-            leading: CircleAvatar(
-              backgroundColor: Colors.blue,
-              child: Text("A"),
-            ),
-            title: Text(this.products[position].name),
-            subtitle: Text(this.products[position].description),
-            onTap: (){},
-          ),
-        );
-      }
+    return Scaffold(
+      body: productList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){goInsertProduct();},
+        child: Icon(Icons.add),
+        tooltip: "Yeni ürün ekle",
+      ),
     );
   }
 
+  ListView productList() {
+    return ListView.builder(
+        itemCount: productValue,
+        itemBuilder: (BuildContext context, int position) {
+          return Card(
+            color: Colors.amberAccent,
+            elevation: 1.5,
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.indigoAccent,
+                child: Text("A"),
+              ),
+              title: Text(this.products[position].name),
+              subtitle: Text(this.products[position].description),
+              onTap: () {
+                goDetail(this.products[position]);
+              },
+            ),
+          );
+        });
+  }
+
   void getProducts() {
-    var dbFuture = dbhelper.createdb();
-    dbFuture.then((result){
-      var productsFuture = dbhelper.getProducts();
-      productsFuture.then((data){
-        List<Urun> productsData = new List<Urun>();
+    var dbFuture = dbHelper.createdb();
+    dbFuture.then((result) {
+      var productsFuture = dbHelper.getProducts();
+      productsFuture.then((data) {
+        List<Product> productsData = new List<Product>();
 
         var incomingProductValue = data.length;
-        for(int i=0; i<incomingProductValue; i++){
-          productsData.add(Urun.fromObject(data[i]));
+        for (int i = 0; i < incomingProductValue; i++) {
+          productsData.add(Product.fromObject(data[i]));
         }
         setState(() {
           products = productsData;
@@ -57,5 +71,24 @@ class ProductListState extends State{
         });
       });
     });
+  }
+
+  void goDetail(Product product) async {
+    bool result = await Navigator.push(context,
+        MaterialPageRoute(builder: (context) => ProductDetail(product)));
+    if (result != null) {
+      if (result) {
+        getProducts();
+      }
+    }
+  }
+
+  void goInsertProduct() async{
+    bool result = await Navigator.push(context, MaterialPageRoute(builder: (context)=> InsertProduct()));
+    if(result != null){
+      if(result){
+        getProducts();
+      }
+    }
   }
 }
--- lib/models/Urun.dart
+++ lib/models/Urun.dart
@@ -1,28 +1,32 @@
-class Urun{
+class Product{
   int _id;
   String _name;
   String _description;
   double _price;
 
-  Urun(this._name, this._description, this._price);
-  Urun.withId(this._id, this._name, this._description, this._price);
+  Product(this._name, this._description, this._price);
+  Product.withId(this._id, this._name, this._description, this._price);
 
   int get id => _id;
+
   set id(int value) {
     _id = value;
   }
 
   String get name => _name;
+
   set name(String value) {
     _name = value;
   }
 
   String get description => _description;
+
   set description(String value) {
     _description = value;
   }
 
   double get price => _price;
+
   set price(double value) {
     _price = value;
   }
@@ -40,10 +44,12 @@ class Urun{
     return map;
   }
 
-  Urun.fromObject(dynamic o){
+  Product.fromObject(dynamic o){
     this._id = o["id"];
     this._name = o["name"];
     this._description = o["description"];
     this._price = double.tryParse(o["price"].toString());
   }
+
+
 }
