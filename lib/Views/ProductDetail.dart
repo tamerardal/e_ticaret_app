d80bde11dccb256a0035fd092a1c2f2bc1402d42
Tamer <tamerardaal@gmail.com>
2021-04-17 12:43:43 +0300

    ürün detay sayfası düzeltildi.

--- lib/Views/ProductDetail.dart
+++ lib/Views/ProductDetail.dart
@@ -2,7 +2,7 @@ import 'package:e_ticaret_app/db/db_Helper.dart';
 import 'package:e_ticaret_app/models/Urun.dart';
 import 'package:flutter/material.dart';
 
-class ProductDetail extends StatefulWidget{
+class ProductDetail extends StatefulWidget {
   Product product;
   ProductDetail(this.product);
 
@@ -14,9 +14,9 @@ class ProductDetail extends StatefulWidget{
 
 DbHelper dbHelper = new DbHelper();
 
-enum Option{delete, update}
+enum Option { delete, update }
 
-class ProductDetailState extends State{
+class ProductDetailState extends State {
   Product product;
   ProductDetailState(this.product);
 
@@ -28,35 +28,56 @@ class ProductDetailState extends State{
         actions: <Widget>[
           PopupMenuButton<Option>(
             onSelected: chooseOp,
-            itemBuilder: (BuildContext context)=><PopupMenuEntry<Option>>[
-            PopupMenuItem<Option>(
-                value: Option.delete,
-              child: Text("Ürünü sil")
-            ),
-            PopupMenuItem<Option>(
-              value: Option.update,
-              child: Text("Ürünü güncelle")
-            )
-          ],
+            itemBuilder: (BuildContext context) => <PopupMenuEntry<Option>>[
+              PopupMenuItem<Option>(
+                  value: Option.delete, child: Text("Ürünü sil")),
+              PopupMenuItem<Option>(
+                  value: Option.update, child: Text("Ürünü güncelle"))
+            ],
           )
         ],
       ),
-      body: Text("Detay", textDirection: TextDirection.ltr),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.shop),
+              title: Text(product.name),
+              subtitle: Text(product.description),
+            ),
+            Text("${product.name} için fiyat: ${product.price} ₺"),
+            ButtonBar(
+              children: <Widget>[
+                FlatButton(
+                    onPressed: () {
+                      Navigator.pop(context, true);
+                      AlertDialog alertDialog = new AlertDialog(
+                        title: Text("İşlem başarılı."),
+                        content: Text("Ürün sepete eklendi!"),
+                      );
+                      showDialog(context: context, builder: (_) => alertDialog);
+                    },
+                    child: Text("Sepete ekle"))
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 
-  void chooseOp(Option option) async{
+  void chooseOp(Option option) async {
     int result;
-    switch(option){
+    switch (option) {
       case Option.delete:
         Navigator.pop(context, true);
         result = await dbHelper.delete(product.id);
-        if(result != 0){
+        if (result != 0) {
           AlertDialog alertDialog = new AlertDialog(
             title: Text("İşlem başarılı"),
             content: Text("Ürün silindi: ${product.name}"),
           );
-          showDialog(context: context, builder: (_) =>  alertDialog);
+          showDialog(context: context, builder: (_) => alertDialog);
         }
         break;
     }
